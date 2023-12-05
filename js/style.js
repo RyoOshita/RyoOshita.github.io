b31d40d29751c82f1fddd58b3d3caa32ed9148bf
Ryo Oshita <bedaka524@gmail.com>
2023-12-05 18:08:26 +0900

    Add プロフィールおよび経歴を学生まで追加

--- js/style.js
+++ js/style.js
@@ -1,13 +1,19 @@
 // スクロールに合わせてheaderを変更する処理
 const pageTop = document.getElementById('top');
 const header = document.getElementById('header');
-const options ={
-    threshold: 0
+const headerul = document.querySelector('header ul')
+const options = {
+    threshold: 0.1
 }
 const observer = new IntersectionObserver(changeHeader, options);
 observer.observe(pageTop);
 
-function changeHeader(){
-    header.style.backgroundcolor = 'ARGB(0,0,0,0)';
-    header.children.style.color = 'var(--main)';
+function changeHeader(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            header.style.backdropFilter = 'blur(2px)';
+        } else {
+            header.style.backdropFilter = 'blur(12px)';
+        }
+    })
 }
